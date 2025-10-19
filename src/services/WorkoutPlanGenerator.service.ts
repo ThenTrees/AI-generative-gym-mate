1cf5a3ad1f6e32de2f7ab8f158e7da3046f57296
Tran Le Thien Tri <thientri.trank17@gmail.com>
2025-10-19 18:07:20 +0700

    refactor: rename file

--- src/services/WorkoutPlanGenerator.service.ts
+++ src/services/WorkoutPlanGenerator.service.ts
@@ -7,7 +7,6 @@ import { PgVectorService } from "./pgVector.service";
 import { DATABASE_CONFIG } from "../configs/database";
 import { logger } from "../utils/logger";
 import { PlanRequest } from "../types/request/planRequest";
-import { Plan } from "../types/model/plan.model";
 import { Goal } from "../types/model/goal.model";
 import { UserProfile } from "../types/model/userProfile.model";
 import {
@@ -21,9 +20,7 @@ import { Exercise } from "../types/model/exercise.model";
 import { PlanDay } from "../types/model/planDay.model";
 import { PlanItem } from "../types/model/planItem.model";
 import { Prescription } from "../types/model/prescription";
-import { PlanStrategy } from "../types/model/planStrategy";
 import { IntensityLevel } from "../types/model/intensityLevel";
-import { WorkoutSplit } from "../types/model/workoutSplit";
 import { SessionStructure } from "../types/model/sessionStructure";
 import { HealthConsideration } from "../types/model/healthConsideration";
 import { VolumeTargets } from "../types/model/volumeTargets";
@@ -37,6 +34,9 @@ import {
   WeeklyProgression,
 } from "../types/model/progressiveOverload";
 import dayjs from "dayjs";
+import { Plan } from "../types/model/plan.model";
+import { PlanStrategy } from "../types/model/planStrategy";
+import { WorkoutSplit } from "../types/model/workoutSplit";
 
 types.setTypeParser(1082, (val) => val);
 
